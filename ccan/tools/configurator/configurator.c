ac4147a2f66242f66646a8b7fae279bcaecbc3de
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-03-15 20:19:51 +1300

    configurator: Remove unneeded if

--- ccan/tools/configurator/configurator.c
+++ ccan/tools/configurator/configurator.c
@@ -543,8 +543,7 @@ static bool run_test(const char *cmd, struct test *test)
 	fclose(outf);
 
 	if (verbose > 1)
-		if (system("cat " INPUT_FILE) == -1)
-			;
+		(void)system("cat " INPUT_FILE);
 
 	newcmd = strdup(cmd);
 
