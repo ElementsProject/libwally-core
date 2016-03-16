1bcd25050530fc0738df314ea44be9332883ef7d
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-03-16 17:24:59 +1300

    Don't forget python 3

--- tools/wordlist_cc.py
+++ tools/wordlist_cc.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+from __future__ import print_function
 import sys
 
 if __name__ == "__main__":
@@ -15,14 +16,14 @@ if __name__ == "__main__":
             lengths.append(lengths[-1] + len(w) + 1)
         idxs = ', '.join(['{0}+{1}'.format(string_name, n) for n in lengths])
 
-        print '/* Generated file - do not edit! */'
-        print '#include <wordlist.h>'
-        print
-        print '   static const char %s[] = "%s";' % (string_name, '\\0'.join(words))
-        print '   static const char *%s_idx[] = {%s};' % (string_name, idxs)
-        print
-        print 'const struct words %s = {' % struct_name
-        print '    {0},'.format(len(words))
-        print '    %s,' % string_name
-        print '    %s_idx' % string_name
-        print '};'
+        print('/* Generated file - do not edit! */')
+        print('#include <wordlist.h>')
+        print()
+        print('   static const char %s[] = "%s";' % (string_name, '\\0'.join(words)))
+        print('   static const char *%s_idx[] = {%s};' % (string_name, idxs))
+        print()
+        print('const struct words %s = {' % struct_name)
+        print('    {0},'.format(len(words)))
+        print('    %s,' % string_name)
+        print('    %s_idx' % string_name)
+        print('};')
