25f2cd25911bb5d46e06360432a638792918fc83
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-03-25 06:38:35 +1300

    Fix up cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 # Clean up all generated files
-
 make distclean
 
-rm -rf bld/
-rm -rf autom4te.cache/
+rm -f */*~
+rm -f *~
 rm -f Makefile.in
 rm -f aclocal.m4
-rm -f compile
-rm -f config.guess
 rm -f config.h.in
-rm -f config.sub
 rm -f configure
-rm -f depcomp
-rm -f install-sh
-rm -f ltmain.sh
-rm -f missing
+rm -f src/*pyc
+rm -f src/test/*pyc
+rm -f src/wallycore.py
+rm -f src/swig_python_wrap.c
 rm -f src/Makefile.in
 rm -f src/config.h.in
-rm -f *~
-rm -f */*~
+rm -f tools/build-aux/compile
+rm -f tools/build-aux/config.guess
+rm -f tools/build-aux/config.sub
+rm -f tools/build-aux/depcomp
+rm -f tools/build-aux/install-sh
+rm -f tools/build-aux/ltmain.sh
+rm -f tools/build-aux/missing
+rm -f tools/build-aux/m4/l*.m4
+rm -rf autom4te.cache/
+rm -rf bld/
