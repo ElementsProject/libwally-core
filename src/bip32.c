3799f69aa6d7eef7e1cdd9385a1c5da389673f49
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-03-28 21:12:59 +1300

    Initial CKDPriv implementation for hardened children

--- src/bip32.c
+++ src/bip32.c
@@ -1,6 +1,7 @@
 #include <include/wally_bip32.h>
 #include "hmac.h"
 #include "ccan/ccan/crypto/sha512/sha512.h"
+#include "ccan/ccan/endian/endian.h"
 /*#include "secp256k1/include/secp256k1.h"*/
 #include <string.h>
 #include <stdint.h>
@@ -43,6 +44,19 @@ static bool child_is_hardened(uint32_t child_num)
     return child_num >= BIP32_INITIAL_HARDENED_CHILD;
 }
 
+static void init_from_sha512(const struct sha512 *sha, struct ext_key *key_out)
+{
+    /* Size of the left and right (key/chain code) parts */
+    const size_t len = sizeof(*sha) / 2;
+
+    /* Copy the key and set its prefix */
+    key_out->key[0] = KEY_PRIVATE;
+    memcpy(key_out->key + 1, sha->u.u8, len);
+
+    /* Copy the chain code */
+    memcpy(key_out->chain_code, sha->u.u8 + len, len);
+}
+
 
 int bip32_key_from_bytes(const unsigned char *bytes_in, size_t len,
                          struct ext_key *key_out)
@@ -59,15 +73,7 @@ int bip32_key_from_bytes(const unsigned char *bytes_in, size_t len,
     if (key_overflow(sha.u.u64) || key_zero(sha.u.u64))
         return -1; /* Out of bounds */
 
-    /* The real size of the left and right (key/chain code) parts */
-    const size_t actual_len = sizeof(struct sha512) / 2;
-
-    /* Copy the key and set its prefix */
-    key_out->key[0] = KEY_PRIVATE;
-    memcpy(key_out->key + 1, sha.u.u8, actual_len);
-
-    /* Copy the chain code */
-    memcpy(key_out->chain_code, sha.u.u8 + actual_len, actual_len);
+    init_from_sha512(&sha, key_out);
 
     key_out->child_num = 0;
     return 0;
@@ -77,18 +83,37 @@ int bip32_key_from_bytes(const unsigned char *bytes_in, size_t len,
 int bip32_key_from_parent(const struct ext_key *key_in, uint32_t child_num,
                           struct ext_key *key_out)
 {
-    key_out->child_num = child_num;
-
     if (key_in->key[0] == KEY_PRIVATE) {
-        /* Private parent -> private child */
-        /* FIXME */
-    }
-    else {
+        /*
+         *  Private parent -> private child:
+         *     CKDpriv((kpar, cpar), i) -> (ki, ci)
+         */
+        if (child_is_hardened(child_num)) {
+            unsigned char buf[sizeof(key_in->key) + sizeof(child_num)];
+            const beint32_t child_num_be = cpu_to_be32(child_num);
+            struct sha512 sha;
+
+            /* Data = 0x00 || ser256(kpar) || ser32(i)) */
+            memcpy(buf, key_in->key, sizeof(key_in->key));
+            memcpy(buf + sizeof(key_in->key),
+                   &child_num_be, sizeof(child_num_be));
+
+            /* I = HMAC-SHA512(Key = cpar, Data) */
+            hmac_sha512(&sha, key_in->chain_code, sizeof(key_in->chain_code),
+                        buf, sizeof(buf));
+            init_from_sha512(&sha, key_out);
+        } else {
+            /* FIXME */
+            return -1;
+        }
+    } else {
         /* Public parent -> public child */
         if (child_is_hardened(child_num))
             return -1; /* Hardened child cannot be made from public parent */
         /* FIXME */
+        return -1;
     }
 
+    key_out->child_num = child_num;
     return 0;
 }
