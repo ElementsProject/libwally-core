7f014021ef7e71704d80cccabdc77402a04a82d7
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-03-29 04:31:22 +1300

    BIP32: First test vector

--- src/test/test_bip32.py
+++ src/test/test_bip32.py
@@ -9,12 +9,17 @@ class BIP32Tests(unittest.TestCase):
         if not hasattr(self, 'bip32_key_from_bytes'):
             util.bind_all(self, util.bip32_funcs)
 
-    def test_from_seed(self):
-        key_out = util.ext_key()
+    def test_bip32_vectors(self):
+
+        # BIP32 Test vector 1, Chain m:
         seed, seed_len = util.make_cbuffer('000102030405060708090a0b0c0d0e0f')
+        key_out = util.ext_key()
         ret = self.bip32_key_from_bytes(seed, seed_len, byref(key_out))
         self.assertEqual(ret, 0)
-        #print hexlify(key_out.key)
+        ext_pub = '0488ADE4000000000000000000873DFF81C02F525623FD1FE5167EAC3A55A049DE3D314BB42EE227FFED37D50800E8F32E723DECF4051AEFAC8E2C93C9C5B214313817CDB01A1494B917C8436B35E77E9D71'
+        ext_pub = ext_pub.lower()
+        encoded = hexlify(key_out.chain_code) + hexlify(key_out.key)
+        self.assertTrue(encoded in ext_pub)
 
 
 if __name__ == '__main__':
