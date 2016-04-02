53276a19c5a2b5ed47c5df9215fb98115a245147
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-04-02 19:39:17 +1300

    Prefer native python types to ctypes functions

--- src/mnemonic.py
+++ src/mnemonic.py
@@ -1,7 +1,5 @@
 """A wallycore version of https://github.com/trezor/python-mnemonic"""
 import wallycore
-from ctypes import create_string_buffer
-from os import urandom
 
 BIP39_ENTROPY_LEN_128 = wallycore.BIP39_ENTROPY_LEN_128
 BIP39_ENTROPY_LEN_160 = wallycore.BIP39_ENTROPY_LEN_160
@@ -14,25 +12,26 @@ class Mnemonic(object):
     def __init__(self, language):
         self.wordlist = wallycore.bip39_get_wordlist(language)
 
+
     @staticmethod
     def list_languages():
         return wallycore.bip39_get_languages().split()
 
 
     def generate(self, strength = wallycore.BIP39_ENTROPY_LEN_128):
+        from os import urandom
         if strength not in [BIP39_ENTROPY_LEN_128, BIP39_ENTROPY_LEN_160,
                             BIP39_ENTROPY_LEN_192, BIP39_ENTROPY_LEN_224,
                             BIP39_ENTROPY_LEN_256]:
             raise ValueError('Invalid strength %d.' % strength)
 
-        buf = create_string_buffer(urandom(strength), strength)
-        return wallycore.bip39_mnemonic_from_bytes(self.wordlist, buf)
+        return self.to_mnemonic(bytearray(urandom(strength)))
 
 
     def to_entropy(self, words):
         if isinstance(words, list):
             words = ' '.join(words)
-        buf = create_string_buffer(BIP39_ENTROPY_LEN_256)
+        buf = bytearray(BIP39_ENTROPY_LEN_256)
         length = wallycore.bip39_mnemonic_to_bytes(self.wordlist, words, buf)
         if length <= 0:
             raise ValueError('Invalid word list. %s' % words)
@@ -48,7 +47,7 @@ class Mnemonic(object):
 
 
     def to_seed(self, mnemonic, passphrase = ''):
-        buf = create_string_buffer(wallycore.BIP39_SEED_LEN_512)
+        buf = bytearray(wallycore.BIP39_SEED_LEN_512)
         if wallycore.bip39_mnemonic_to_seed(mnemonic, passphrase, buf) == 0:
             raise ValueError('Unable to create seed')
         return bytearray(buf)[0:wallycore.BIP39_SEED_LEN_512]
