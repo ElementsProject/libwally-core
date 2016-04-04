a5796e9e9982364c73cdca446c415a48a07be073
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-04-04 18:25:32 +1200

    BIP38: Minimal build changes

---
+++ include/wally_bip38.h
@@ -0,0 +1,9 @@
+#ifndef LIBWALLY_CORE_BIP38_H
+#define LIBWALLY_CORE_BIP38_H
+
+#include "wally-core.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif /* LIBWALLY_CORE_BIP39_H */
---
+++ src/bip38.c
@@ -0,0 +1,4 @@
+#include <include/wally_bip38.h>
+#include "internal.h"
+#include "ccan/ccan/crypto/sha256/sha256.h"
+
