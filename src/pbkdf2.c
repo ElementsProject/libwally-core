421f1968ec3052b3c444b69101461e5086b65f09
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-04-07 04:03:20 +1200

    Use the output buffer directly if possible in PBKDF2

--- src/pbkdf2.c
+++ src/pbkdf2.c
@@ -17,6 +17,16 @@
 #include "ccan/ccan/crypto/sha256/sha256.h"
 #include "ccan/ccan/crypto/sha512/sha512.h"
 #include "ccan/ccan/build_assert/build_assert.h"
+#include <ccan/compiler/compiler.h>
+
+static bool alignment_ok(const void *p UNUSED, size_t n UNUSED)
+{
+#if HAVE_UNALIGNED_ACCESS
+    return true;
+#else
+    return ((size_t)p % n == 0);
+#endif
+}
 
 
 int pbkdf2_hmac_sha512(const unsigned char *pass, size_t pass_len,
@@ -24,7 +34,7 @@ int pbkdf2_hmac_sha512(const unsigned char *pass, size_t pass_len,
                        size_t cost,
                        unsigned char *bytes_out, size_t len)
 {
-    struct sha512 d1, d2;
+    struct sha512 d1, d2, *sha_cp = &d2;
     size_t n, c, j;
 
     BUILD_ASSERT(sizeof(beint32_t) == PBKDF2_SALT_BYTES);
@@ -36,19 +46,27 @@ int pbkdf2_hmac_sha512(const unsigned char *pass, size_t pass_len,
     if (!len || len % PBKDF2_HMAC_SHA512_LEN)
         return -1;
 
+    /* If bytes out is suitably aligned, we can work on it directly */
+    if(alignment_ok(bytes_out, sizeof(uint64_t)))
+        sha_cp = (struct sha512 *)bytes_out;
+
     for (n = 0; n < len / PBKDF2_HMAC_SHA512_LEN; ++n) {
         beint32_t block = cpu_to_be32(n + 1); /* Block number */
 
         memcpy(salt + salt_len - sizeof(block), &block, sizeof(block));
         hmac_sha512(&d1, pass, pass_len, salt, salt_len);
-        d2 = d1;
+        memcpy(sha_cp, &d1, sizeof(d1));
 
         for (c = 0; cost && c < cost - 1; ++c) {
             hmac_sha512(&d1, pass, pass_len, d1.u.u8, sizeof(d1));
             for (j = 0; j < sizeof(d1); ++j)
-                d2.u.u8[j] ^= d1.u.u8[j];
+                sha_cp->u.u8[j] ^= d1.u.u8[j];
         }
-        memcpy(bytes_out, d2.u.u8, sizeof(d2));
+        if (sha_cp == &d2)
+            memcpy(bytes_out, sha_cp->u.u8, sizeof(*sha_cp));
+        else
+            sha_cp += PBKDF2_HMAC_SHA512_LEN;
+
         bytes_out += PBKDF2_HMAC_SHA512_LEN;
     }
 
