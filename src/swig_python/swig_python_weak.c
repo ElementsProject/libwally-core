aafc1c6a913d67860645ad7eb6e171e692071ee7
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-10-07 00:27:39 +1300

    Fix warnings about python weak symbol mismatches

--- src/swig_python/swig_python_weak.c
+++ src/swig_python/swig_python_weak.c
@@ -21,70 +21,122 @@
 #define DUMMY_VARIABLE_WEAKREF(var) \
     WALLY_CORE_API void *var __attribute__((weak));
 
-DUMMY_WEAKREF(PyArg_ParseTuple)
-DUMMY_WEAKREF(PyArg_UnpackTuple)
-DUMMY_WEAKREF(PyBool_FromLong)
-DUMMY_WEAKREF(PyCapsule_GetPointer)
-DUMMY_WEAKREF(PyCapsule_Import)
-DUMMY_WEAKREF(PyCapsule_New)
-DUMMY_WEAKREF(PyDict_GetItem)
-DUMMY_WEAKREF(PyDict_New)
-DUMMY_WEAKREF(PyDict_SetItem)
-DUMMY_WEAKREF(PyDict_SetItemString)
+WALLY_CORE_API int PyArg_ParseTuple(void *x, void *y, ...) __attribute__((weak));
+int PyArg_ParseTuple(void *x, void *y, ...) { (void)x; (void)y; return 0; }
+WALLY_CORE_API int PyArg_UnpackTuple(void *x, void* y, size_t z, size_t z2, ...) __attribute__((weak));
+int PyArg_UnpackTuple(void *x, void* y, size_t z, size_t z2, ...) { (void)x; (void)y; (void)z; (void)z2; return 0; }
+WALLY_CORE_API void * PyBool_FromLong(long x) __attribute__((weak));
+void * PyBool_FromLong(long x) { (void)x; return NULL; }
+
+WALLY_CORE_API void* PyCapsule_GetPointer(void *x, void *y) __attribute__((weak));
+void* PyCapsule_GetPointer(void *x, void *y) { (void)x; return y; }
+WALLY_CORE_API void* PyCapsule_Import(void *x, int y) __attribute__((weak));
+void* PyCapsule_Import(void *x, int y) { (void)y; return x; }
+WALLY_CORE_API void* PyCapsule_New(void *x, void* y, void *z) __attribute__((weak));
+void* PyCapsule_New(void *x, void* y, void *z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API void* PyDict_GetItem(void *x, void *y) __attribute__((weak));
+void* PyDict_GetItem(void *x, void *y) { (void)x; return y; }
+
+WALLY_CORE_API void * PyDict_New(void) __attribute__((weak));
+void * PyDict_New(void) { return NULL; }
+
+WALLY_CORE_API int PyDict_SetItem(void *x, void *y, void *z) __attribute__((weak));
+int PyDict_SetItem(void *x, void *y, void *z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API int PyDict_SetItemString(void *x, void *y, void *z) __attribute__((weak));
+int PyDict_SetItemString(void *x, void *y, void *z) { (void)x; (void)y; (void)z; return 0; }
 DUMMY_WEAKREF(PyErr_Clear)
 DUMMY_WEAKREF(PyErr_Fetch)
 DUMMY_WEAKREF(PyErr_Format)
 DUMMY_WEAKREF(PyErr_Occurred)
 DUMMY_WEAKREF(PyErr_Restore)
 DUMMY_WEAKREF(PyErr_SetObject)
-DUMMY_WEAKREF(PyErr_SetString)
+WALLY_CORE_API void PyErr_SetString(void *x, void *y) __attribute__((weak));
+void PyErr_SetString(void *x, void *y) { (void)x; (void)y; }
 DUMMY_WEAKREF(PyErr_WriteUnraisable)
 DUMMY_WEAKREF(PyFloat_AsDouble)
-DUMMY_WEAKREF(PyInstance_NewRaw)
-DUMMY_WEAKREF(PyInt_AsLong)
-DUMMY_WEAKREF(PyInt_FromLong)
-DUMMY_WEAKREF(PyInt_FromSize_t)
+WALLY_CORE_API void* PyInstance_NewRaw(void *x, void *y) __attribute__((weak));
+void* PyInstance_NewRaw(void *x, void *y) { (void)x; return y; }
+WALLY_CORE_API long PyInt_AsLong(void *x) __attribute__((weak));
+long PyInt_AsLong(void *x) { (void)x; return 0; }
+WALLY_CORE_API void * PyInt_FromLong(long x) __attribute__((weak));
+void * PyInt_FromLong(long x) { (void)x; return NULL; }
+WALLY_CORE_API void * PyInt_FromSize_t(size_t x) __attribute__((weak));
+void * PyInt_FromSize_t(size_t x) { (void)x; return NULL; }
 DUMMY_WEAKREF(PyList_Append)
 DUMMY_WEAKREF(PyList_New)
 DUMMY_WEAKREF(PyList_SetItem)
 DUMMY_WEAKREF(PyLong_AsDouble)
-DUMMY_WEAKREF(PyLong_AsUnsignedLong)
+WALLY_CORE_API unsigned long PyLong_AsUnsignedLong(void *x) __attribute__((weak));
+unsigned long PyLong_AsUnsignedLong(void *x) { (void)x; return 0; }
 DUMMY_WEAKREF(PyLong_AsUnsignedLongLong)
 DUMMY_WEAKREF(PyLong_FromLong)
 DUMMY_WEAKREF(PyLong_FromUnsignedLong)
-DUMMY_WEAKREF(PyLong_FromVoidPtr)
-DUMMY_WEAKREF(PyModule_AddObject)
-DUMMY_WEAKREF(PyModule_GetDict)
+WALLY_CORE_API void * PyLong_FromVoidPtr(void *x) __attribute__((weak));
+void * PyLong_FromVoidPtr(void *x) { return x; }
+WALLY_CORE_API int PyModule_AddObject(void *x, void *y, void *z) __attribute__((weak));
+int PyModule_AddObject(void *x, void *y, void *z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API void * PyModule_GetDict(void *x) __attribute__((weak));
+void * PyModule_GetDict(void *x) { return x; }
+
 DUMMY_WEAKREF(PyOS_snprintf)
-DUMMY_WEAKREF(PyObject_AsReadBuffer)
-DUMMY_WEAKREF(PyObject_AsWriteBuffer)
-DUMMY_WEAKREF(PyObject_Call)
+WALLY_CORE_API int PyObject_AsReadBuffer(void *x, void* y, void *z) __attribute__((weak));
+int PyObject_AsReadBuffer(void *x, void* y, void *z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API int PyObject_AsWriteBuffer(void *x, void* y, void* z) __attribute__((weak));
+int PyObject_AsWriteBuffer(void *x, void* y, void* z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API void * PyObject_Call(void *x, void *y, void *z) __attribute__((weak));
+void * PyObject_Call(void *x, void *y, void *z) { (void)x; (void)y; (void)z; return NULL; }
 DUMMY_WEAKREF(PyObject_CallFunctionObjArgs)
-DUMMY_WEAKREF(PyObject_Free)
-DUMMY_WEAKREF(PyObject_GenericGetAttr)
-DUMMY_WEAKREF(PyObject_GetAttr)
+WALLY_CORE_API void PyObject_Free(void *x) __attribute__((weak));
+void PyObject_Free(void *x) { (void)x; }
+
+WALLY_CORE_API void* PyObject_GenericGetAttr(void *x, void *y) __attribute__((weak));
+void* PyObject_GenericGetAttr(void *x, void *y) { (void)x; return y; }
+WALLY_CORE_API void* PyObject_GetAttr(void *x, void *y) __attribute__((weak));
+void* PyObject_GetAttr(void *x, void *y) { (void)x; return y; }
+
 DUMMY_WEAKREF(PyObject_GetAttrString)
-DUMMY_WEAKREF(PyObject_Init)
-DUMMY_WEAKREF(PyObject_IsTrue)
-DUMMY_WEAKREF(PyObject_Malloc)
+WALLY_CORE_API void* PyObject_Init(void *x, void *y) __attribute__((weak));
+void* PyObject_Init(void *x, void *y) { (void)x; return y; }
+WALLY_CORE_API int PyObject_IsTrue(void *x) __attribute__((weak));
+int PyObject_IsTrue(void *x) { (void)x; return 0; }
+WALLY_CORE_API void* PyObject_Malloc(size_t x) __attribute__((weak));
+void* PyObject_Malloc(size_t x) { (void)x; return NULL; }
+WALLY_CORE_API void Py_DecRef(void* x) __attribute__((weak));
+void Py_DecRef(void* x) { (void)x; }
 DUMMY_WEAKREF(PyObject_Str)
-DUMMY_WEAKREF(PyString_AsString)
-DUMMY_WEAKREF(PyString_AsStringAndSize)
-DUMMY_WEAKREF(PyString_ConcatAndDel)
-DUMMY_WEAKREF(PyString_Format)
+WALLY_CORE_API void * PyString_AsString(void *x) __attribute__((weak));
+void * PyString_AsString(void *x) { return x; }
+WALLY_CORE_API int PyString_AsStringAndSize(void *x, void* y, void *z) __attribute__((weak));
+int PyString_AsStringAndSize(void *x, void* y, void *z) { (void)x; (void)y; (void)z; return 0; }
+WALLY_CORE_API void PyString_ConcatAndDel(void *x, void *y) __attribute__((weak));
+void PyString_ConcatAndDel(void *x, void *y) { (void)x; (void)y; }
+
+WALLY_CORE_API void* PyString_Format(void *x, void *y) __attribute__((weak));
+void* PyString_Format(void *x, void *y) { (void)x; return y; }
 DUMMY_WEAKREF(PyString_FromFormat)
-DUMMY_WEAKREF(PyString_FromString)
+WALLY_CORE_API void * PyString_FromString(void *x) __attribute__((weak));
+void * PyString_FromString(void *x) { return x; }
 DUMMY_WEAKREF(PyString_FromStringAndSize)
-DUMMY_WEAKREF(PyTuple_New)
-DUMMY_WEAKREF(PyTuple_SetItem)
+WALLY_CORE_API void * PyTuple_New(size_t x) __attribute__((weak));
+void * PyTuple_New(size_t x) { (void)x; return NULL; }
+WALLY_CORE_API int PyTuple_SetItem(void *x, size_t y, void *z) __attribute__((weak));
+int PyTuple_SetItem(void *x, size_t y, void *z) { (void)x; (void)y; (void)z; return 0; }
 DUMMY_WEAKREF(PyType_IsSubtype)
-DUMMY_WEAKREF(PyType_Ready)
-DUMMY_WEAKREF(Py_DecRef)
-DUMMY_WEAKREF(Py_IncRef)
-DUMMY_WEAKREF(Py_InitModule4_64)
-DUMMY_WEAKREF(_PyInstance_Lookup)
-DUMMY_WEAKREF(_PyObject_GetDictPtr)
-DUMMY_WEAKREF(_PyObject_New)
+WALLY_CORE_API int PyType_Ready(void *x) __attribute__((weak));
+int PyType_Ready(void *x) { (void)x; return 0; }
+WALLY_CORE_API void Py_IncRef(void *x) __attribute__((weak));
+void Py_IncRef(void *x) { (void)x; }
+WALLY_CORE_API void* Py_InitModule4_64(void *x, void* y, void *z, void *z2, int z3) __attribute__((weak));
+void* Py_InitModule4_64(void *x, void* y, void *z, void *z2, int z3) { (void)x; (void)y; (void)z; (void)z2; (void)z3; return NULL; }
+WALLY_CORE_API void* _PyInstance_Lookup(void *x, void *y) __attribute__((weak));
+void* _PyInstance_Lookup(void *x, void *y) { (void)x; return y; }
+WALLY_CORE_API void * _PyObject_GetDictPtr(void *x) __attribute__((weak));
+void * _PyObject_GetDictPtr(void *x) { return x; }
+
+WALLY_CORE_API void * _PyObject_New(void *x) __attribute__((weak));
+void * _PyObject_New(void *x) { return x; }
+
+
 DUMMY_VARIABLE_WEAKREF(PyCFunction_Type)
 DUMMY_VARIABLE_WEAKREF(PyClass_Type)
 DUMMY_VARIABLE_WEAKREF(PyFloat_Type)
