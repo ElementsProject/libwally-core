5ab414ff49cd834ac71082b02e3ae4224366dd33
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-12-09 15:06:05 +1300

    Add commented out signing to python build script

--- tools/build_python_eggs.sh
+++ tools/build_python_eggs.sh
@@ -19,6 +19,7 @@ function build {
     mv dist $NAME-$1
     tar czf $NAME-$1.tar.gz $NAME-$1
     sha256sum $NAME-$1.tar.gz >$NAME-$1.tar.gz.sha256
+    #gpg --armor --output $NAME-$1.tar.gz.asc --detach-sign $NAME-$1.tar.gz
     rm -r $NAME-$1
     deactivate
 }
