b4c7a09ed2df71c642e43b353b9d4f4db15214e2
Lawrence Nahum <lawrence@greenaddress.it>
2018-02-13 11:45:34 +0100

    add CI support for debug build

--- tools/build_js_bindings.sh
+++ tools/build_js_bindings.sh
@@ -4,11 +4,14 @@ set -e
 
 tools/cleanup.sh
 tools/autogen.sh
-./configure --enable-js-wrappers --disable-swig-python --disable-swig-java
+./configure --enable-js-wrappers --disable-swig-python --disable-swig-java $DEBUG_WALLY
 num_jobs=4
 if [ -f /proc/cpuinfo ]; then
     num_jobs=$(grep ^processor /proc/cpuinfo | wc -l)
 fi
 make -o configure clean
 make -o configure -j $num_jobs
+# test that make twice works
+make -o configure
+
 ./tools/cleanup.sh
