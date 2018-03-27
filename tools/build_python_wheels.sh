fcab1f00d3fc1f05ced53de69f390b8b7e44b826
Jeremy Kauffman <jeremy@blockstream.io>
2018-03-27 08:51:06 +0100

    Perform smoketest of built wheel

--- tools/build_python_wheels.sh
+++ tools/build_python_wheels.sh
@@ -12,13 +12,28 @@ set -e
 function build_wheel {
     ./tools/cleanup.sh
 
+    # Create a new virtualenv and use it to build the wheel using pip wheel
     virtualenv -p $1 .venv
     source .venv/bin/activate
 
     pip install wheel
-    pip wheel .
+    pip wheel --wheel-dir=.venv .
 
     deactivate
+
+    # Smoke test the built wheel by installing it into a new virtualenv
+    virtualenv -p $1 .venv/.smoketest
+    source .venv/.smoketest/bin/activate
+
+    pip install --upgrade pip
+    pip install .venv/*.whl
+    python -c "import wallycore as wally; assert wally.hex_from_bytes(wally.hex_to_bytes('ff')) == 'ff';"
+
+    deactivate
+
+    # Copy the wheel into the root directory and remove the virtualenvs
+    cp .venv/*.whl .
+    rm -rf .venv
 }
 
 build_wheel python2
