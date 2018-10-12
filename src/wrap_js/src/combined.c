fb8162abafdac6ee65bf83a0ef4fe5e725abee5a
Jon Griffiths <jon_p_griffiths@yahoo.com>
2018-10-12 21:03:13 +1300

    JS: silence unused secp function warnings

--- src/wrap_js/src/combined.c
+++ src/wrap_js/src/combined.c
@@ -34,6 +34,10 @@ void wally_silence_unused_warnings(void)
     secp256k1_ge_set_all_gej_var(NULL, NULL, 0, NULL);
     secp256k1_gej_has_quad_y_var(NULL);
     secp256k1_gej_is_valid_var(NULL);
+    secp256k1_ge_set_infinity(NULL);
+    secp256k1_ecmult_multi_var(NULL, NULL, NULL, NULL, NULL, NULL, 0);
+    secp256k1_ecmult_strauss_batch_single(NULL, NULL, NULL, NULL, NULL, NULL, 0);
+    secp256k1_ecmult_pippenger_batch_single(NULL, NULL, NULL, NULL, NULL, NULL, 0);
     tx_elements_input_issuance_proof_init(NULL, NULL, 0, NULL, 0);
     tx_elements_output_proof_init(NULL, NULL, 0, NULL, 0);
     witness_stack_from_bytes(NULL, NULL, NULL);
