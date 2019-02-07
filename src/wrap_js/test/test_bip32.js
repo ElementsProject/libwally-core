eabb9343c798747a26ee25d713941c071a4cb9d9
Luca Vaccaro <luck87@gmail.com>
2019-02-07 16:41:37 +0100

    Migrate example to test

--- src/wrap_js/test/test_bip32.js
+++ src/wrap_js/test/test_bip32.js
@@ -1,13 +1,11 @@
 var wally = require('../wally');
 var test = require('tape');
+const EC_PUBLIC_KEY_LEN = 33;
+var seed = Buffer.from('00000000000000000000000000000000', 'hex');
 
 test('BIP32 from seed + derivation', function(t) {
-  t.plan(2);
-  var zeroes = [];
-  for (var i = 0; i < 16; ++i) {
-    zeroes.push(0);
-  }
-  wally.bip32_key_from_seed(Buffer.from(zeroes), 0x0488ADE4, 0).then(function(s) {
+  t.plan(7);
+  wally.bip32_key_from_seed(Buffer.from(seed), 0x0488ADE4, 0).then(function(s) {
     wally.wally_base58_from_bytes(s, 1).then(function (s) {
       t.equal(
         s,
@@ -16,6 +14,7 @@ test('BIP32 from seed + derivation', function(t) {
         'privkey'
       );
     });
+
     wally.bip32_pubkey_from_parent(s, 1, 0).then(function (pub) {
       wally.wally_base58_from_bytes(pub, 1).then(function (s) {
         t.equal(
@@ -26,5 +25,74 @@ test('BIP32 from seed + derivation', function(t) {
         );
       });
     });
+
+    wally.wally_ec_public_key_from_private_key(s.slice(46, 78)).then(function(master_pubkey) {
+      t.equal(
+        Buffer.from(master_pubkey).toString('hex'),
+        '02be99138b48b430a8ee40bf8b56c8ebc584c363774010a9bfe549a87126e61746',
+        'M/0'
+      );
+    });
+
+    wally.bip32_privkey_from_parent(s, 0, 0).then(function (xpriv_0_0) {
+      wally.wally_base58_from_bytes(xpriv_0_0, 1).then(function (base58_xpriv) {
+        t.equal(
+          base58_xpriv,
+          'xprv9u4S6Taa3k3GxnaHfWzboKwLPPPHpDyDHdLGqDArBejguBuv6GkerLy6MtAeFfo9RDfZy22FWEc1ExEShuRGZJpgVgeVu5KZ5obWbV2R3D2',
+          'xpriv m/0/0'
+        );
+      });
+    });
+
+    wally.bip32_pubkey_from_parent(s, 0, 0).then(function (xpub_0_0) {
+      wally.wally_base58_from_bytes(xpub_0_0, 1).then(function (base58_xpub) {
+        t.equal(
+          base58_xpub,
+          'xpub683nVy7Tt7baBGekmYXcATt4wRDnDgh4erFsdbaTjzGfmzF4dp4uQ9HaDCdvSqctrsbxZey5wozKyyy2J3zhDDHU3UhW4uCFQp6bESv8ewQ',
+          'xpub M/0/0'
+        );
+      });
+
+      wally.bip32_pubkey_from_parent(xpub_0_0, 1, 1).then(function (xpub_0_0_1) {
+        wally.wally_base58_from_bytes(xpub_0_0_1, 1).then(function (base58_xpub) {
+          t.equal(
+            base58_xpub,
+            'xpub6An6e2ai6kSDnnxJ3876JwfeigdQu9YNudcP7ayT828xDFzFQkP9oBoBNdvj7xDrDQd9TQDpzkLhM5L71rFDTmxMuzSvXwZKnLx56Es6MEg',
+            'xpub M/0/0/1'
+          );
+        });
+
+        var version = Buffer.from('0014', 'hex');
+        wally.wally_hash160(xpub_0_0_1.slice(45, 78)).then((hash160) => {
+          return wally.wally_addr_segwit_from_bytes(Buffer.concat([version, Buffer.from(hash160)]),'tb',0);
+        }).then((addr) => {
+          t.equal(
+            addr,
+            'tb1q6pqwl5tyaluz8qtqky7x2cmxzqdr6gsyld4hrn',
+            'bech32: addr'
+          );
+        });
+      });
+    });
+  });
+});
+
+test('BIP32 from seed to address', function(t) {
+  t.plan(1);
+
+  wally.bip32_key_from_seed(seed, 0x0488ADE4, 0).then(function(s) {
+    return wally.bip32_pubkey_from_parent(s, 0, 1);
+  }).then((xpubkey) => {
+    const pubkey = xpubkey.slice(45, 78);
+    return wally.wally_hash160(pubkey);
+  }).then((script) => {
+    const prefix = Buffer.from('eb', 'hex');
+    return wally.wally_base58_from_bytes(Buffer.concat([prefix, script]), 1);
+  }).then((address) => {
+    t.equal(
+      address,
+      '2dmvtD27wpRyLK79FsAidyS33uUogsYNC4U',
+      'address'
+    );
   });
 });
