f27740564955e9a63d2946bdb0099622c0977b6e
Lawrence Nahum <lawrence@greenaddress.it>
2019-02-12 18:32:45 +0100

    Update to release 0.6.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ author = u'Jon Griffiths'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.6.7'
+version = u'0.6.8'
 # The full version, including alpha/beta/rc tags.
 release = version
 
