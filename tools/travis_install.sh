d2b1206f82bd75f6c84c8326178ccc5c18052c91
Lawrence Nahum <lawrence@greenaddress.it>
2019-05-31 21:26:48 +0200

    use supported version of swig for osx

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -3,5 +3,6 @@
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew update
     brew install gnu-sed
-    brew install swig yarn
+    brew install swig@3.0.4 yarn
+    brew link --overwrite swig@3.0.4
 fi
