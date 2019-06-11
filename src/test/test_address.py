b14a541b0d246e07c74e3179234b9f2b58bfc0e0
Sjors Provoost <sjors@sprovoost.nl>
2019-06-11 17:24:22 +0200

    Address derivation testnet vectors

--- src/test/test_address.py
+++ src/test/test_address.py
@@ -11,8 +11,13 @@ ADDRESS_TYPE_P2PKH       = 0x01
 ADDRESS_TYPE_P2SH_P2WPKH = 0x02
 ADDRESS_TYPE_P2WPKH      = 0x04
 
-VERSION_P2PKH = 0x00
-VERSION_P2SH = 0x05
+VERSION_P2PKH_MAINNET = 0x00
+VERSION_P2PKH_TESTNET = 0x6F
+VERSION_P2SH_MAINNET = 0x05
+VERSION_P2SH_TESTNET = 0xC4
+
+NETWORK_BITCOIN_MAINNET = 0x01
+NETWORK_BITCOIN_TESTNET = 0x02
 
 # Vector from test_bip32.py. We only need an xpub to derive addresses.
 vec = {
@@ -29,7 +34,25 @@ vec = {
 
         "address_p2sh_segwit": '3DymAvEWH38HuzHZ3VwLus673bNZnYwNXu',
 
-        "address_segwit": 'bc1qhm6697d9d2224vfyt8mj4kw03ncec7a7fdafvt'
+        "address_segwit": 'bc1qhm6697d9d2224vfyt8mj4kw03ncec7a7fdafvt',
+    },
+
+    'm/1H/1': {
+        # tpubDApXh6cD2fZ7WjtgpHd8yrWyYaneiFuRZa7fVjMkgxsmC1QzoXW8cgx9zQFJ81Jx4deRGfRE7yXA9A3STsxXj4CKEZJHYgpMYikkas9DBTP
+        FLAG_KEY_PUBLIC:  '043587CF025C1BD648000000012A7857'
+                          '631386BA23DACAC34180DD1983734E44'
+                          '4FDBF774041578E9B6ADB37C1903501E'
+                          '454BF00751F24B1B489AA925215D66AF'
+                          '2234E3891C3B21A52BEDB3CD711C6F6E2AF7',
+
+        "address_legacy": 'mxvewdhKCenLkYgNa8irv1UM2omEWPMdEE',
+
+
+        "address_p2sh_segwit": '2N5XyEfAXtVde7mv6idZDXp5NFwajYEj9TD',
+
+
+        "address_segwit": 'tb1qhm6697d9d2224vfyt8mj4kw03ncec7a7rtx6hc',
+
     }
 
 }
@@ -51,31 +74,35 @@ class AddressTests(unittest.TestCase):
         return key_out
 
     def test_address_vectors(self):
-        self.do_test_vector(vec, 'm/0H/1')
+        self.do_test_vector(vec, 'm/0H/1', NETWORK_BITCOIN_MAINNET)
+        self.do_test_vector(vec, 'm/1H/1', NETWORK_BITCOIN_TESTNET) # Testnet
 
-    def do_test_vector(self, vec, path):
+    def do_test_vector(self, vec, path, network):
         key = self.get_test_key(vec, path)
 
         # Address type flag is mandatory
-        ret, out = wally_bip32_key_to_address(key, 0, VERSION_P2PKH)
+        version = VERSION_P2PKH_MAINNET if network == NETWORK_BITCOIN_MAINNET else VERSION_P2PKH_TESTNET
+        ret, out = wally_bip32_key_to_address(key, 0, version)
         self.assertEqual(ret, WALLY_EINVAL)
 
         # Obtain legacy address (P2PKH)
-        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2PKH, VERSION_P2PKH)
+        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2PKH, version)
         self.assertEqual(ret, WALLY_OK)
         self.assertEqual(out, vec[path]['address_legacy'])
 
         # Obtain wrapped SegWit address (P2SH_P2WPKH)
-        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2SH_P2WPKH, VERSION_P2SH)
+        version = VERSION_P2SH_MAINNET if network == NETWORK_BITCOIN_MAINNET else VERSION_P2SH_TESTNET
+        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2SH_P2WPKH, version)
         self.assertEqual(ret, WALLY_OK)
         self.assertEqual(out, vec[path]['address_p2sh_segwit'])
 
         # wally_bip32_key_to_address does not support bech32 native SegWit (P2WPKH)
-        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2WPKH, VERSION_P2SH)
+        ret, out = wally_bip32_key_to_address(key, ADDRESS_TYPE_P2WPKH, version)
         self.assertEqual(ret, WALLY_EINVAL)
 
         # Obtain native SegWit address (P2WPKH)
-        ret, out = wally_bip32_key_to_addr_segwit(key, utf8('bc'), 0)
+        bech32_prefix = 'bc' if network == NETWORK_BITCOIN_MAINNET else 'tb'
+        ret, out = wally_bip32_key_to_addr_segwit(key, utf8(bech32_prefix), 0)
         self.assertEqual(ret, WALLY_OK)
         self.assertEqual(out, vec[path]['address_segwit'])
 
