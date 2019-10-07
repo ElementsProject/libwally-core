6ca0820a1a81a786239c175f60c676c9980aab3e
Goncalo Carvalho <goncalo@blockstream.io>
2019-10-07 10:52:43 +0100

    add fixed versions for windows

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -5,5 +5,8 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew install gnu-sed
     brew install swig yarn
 elif [ "$TRAVIS_OS_NAME" = "windows" ]; then
-    choco install swig python
+    SWIG_VERSION="4.0.1"
+    choco install swig --version $SWIG_VERSION
+    choco install python --version 3.7.4
+    ln -sf /c/ProgramData/chocolatey/lib/swig/tools/install/swigwin-$SWIG_VERSION /c/swig
 fi
