dbb5e11a76fc4e09ad6381ce695f740bc536ed9b
Leonardo Comandini <leo@leocomandini.org>
2019-12-10 17:48:37 +0100

    bump version to 0.7.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 kwargs = {
     'name': 'wallycore',
-    'version': '0.7.5',
+    'version': '0.7.6',
     'description': 'libwally Bitcoin library',
     'long_description': 'Python bindings for the libwally Bitcoin library',
     'url': 'https://github.com/ElementsProject/libwally-core',
