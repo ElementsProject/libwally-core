4eeea0ba136bebb3e8d55b898099e6c14e69daef
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-03-06 11:35:20 +0900

    feat: output descriptor fix: msvc ELEMENTS_BUILD empty case

--- src/test/util.py
+++ src/test/util.py
@@ -46,6 +46,15 @@ class ext_key(Structure):
                 ('pub_key', c_ubyte * 33),
                 ('pub_key_tweak_sum', c_ubyte * 32)]
 
+class wally_descriptor_address_item(Structure):
+    _fields_ = [('child_num', c_uint),
+                ('address', c_char_p),
+                ('address_len', c_ulong)]
+
+class wally_descriptor_addresses(Structure):
+    _fields_ = [('items',  POINTER(wally_descriptor_address_item)),
+                ('num_items', c_ulong)]
+
 # Sentinel classes for returning output parameters
 class c_char_p_p_class(object):
     pass
@@ -330,7 +339,12 @@ for f in (
     ('wally_combine_psbts', c_int, [POINTER(wally_psbt), c_ulong, POINTER(POINTER(wally_psbt))]),
     ('wally_sign_psbt', c_int, [POINTER(wally_psbt), c_void_p, c_ulong]),
     ('wally_finalize_psbt', c_int, [POINTER(wally_psbt)]),
-    ('wally_extract_psbt', c_int, [POINTER(wally_psbt), POINTER(POINTER(wally_tx))])
+    ('wally_extract_psbt', c_int, [POINTER(wally_psbt), POINTER(POINTER(wally_tx))]),
+    ('wally_parse_miniscript', c_int, [c_char_p, c_void_p, c_void_p, c_ulong, c_uint, c_uint, c_void_p, c_ulong, c_ulong_p]),
+    ('wally_descriptor_to_scriptpubkey', c_int, [c_char_p, c_void_p, c_void_p, c_ulong, c_uint, c_uint, c_uint, c_uint, c_uint, c_void_p, c_ulong, c_ulong_p]),
+    ('wally_descriptor_to_address', c_int, [c_char_p, c_void_p, c_void_p, c_ulong, c_uint, c_uint, c_uint, c_char_p_p]),
+    ('wally_descriptor_to_addresses', c_int, [c_char_p, c_void_p, c_void_p, c_ulong, c_uint, c_uint, c_uint, c_uint, POINTER(wally_descriptor_addresses)]),
+    ('wally_create_descriptor_checksum', c_int, [c_char_p, c_void_p, c_void_p, c_ulong, c_uint, c_char_p_p]),
     ):
 
     def bind_fn(name, res, args):
--- tools/msvc/build.bat
+++ tools/msvc/build.bat
@@ -11,7 +11,7 @@ REM everything is being munged together for Windows as a hack it causes
 REM problems. Make a copy called hex_.c as a workaround.
 copy src\ccan\ccan\str\hex\hex.c src\ccan\ccan\str\hex\hex_.c
 
-if %ELEMENTS_BUILD% == "elements" (
+if "%ELEMENTS_BUILD%" == "elements" (
   set ELEMENTS_OPT=/DBUILD_ELEMENTS
 ) else (
   set ELEMENTS_OPT=
@@ -20,4 +20,4 @@ if %ELEMENTS_BUILD% == "elements" (
 REM Compile everything (wally, ccan, libsecp256k) in one lump.
 REM Define USE_ECMULT_STATIC_PRECOMPUTATION  to pick up the
 REM ecmult_static_context.h file generated previously
-cl /utf-8 /DUSE_ECMULT_STATIC_PRECOMPUTATION /DECMULT_WINDOW_SIZE=16 /DWALLY_CORE_BUILD %ELEMENTS_OPT% /DHAVE_CONFIG_H /DSECP256K1_BUILD /I%LIBWALLY_DIR%\src\wrap_js\windows_config /I%LIBWALLY_DIR% /I%LIBWALLY_DIR%\src /I%LIBWALLY_DIR%\include /I%LIBWALLY_DIR%\src\ccan /I%LIBWALLY_DIR%\src\ccan\base64 /I%LIBWALLY_DIR%\src\secp256k1 /Zi /LD src/aes.c src/base58.c src/bech32.c src/bip32.c src/bip38.c src/bip39.c src/blech32.c src/ecdh.c src/elements.c src/hex.c src/hmac.c src/internal.c src/mnemonic.c src/pbkdf2.c src/psbt.c src/script.c src/scrypt.c src/sign.c src/symmetric.c src/transaction.c src/wif.c src/wordlist.c src/ccan/ccan/crypto/ripemd160/ripemd160.c src/ccan/ccan/crypto/sha256/sha256.c src/ccan/ccan/crypto/sha512/sha512.c src/ccan/ccan/base64/base64.c src\ccan\ccan\str\hex\hex_.c src/secp256k1/src/secp256k1.c /Fewally.dll
+cl /utf-8 /DUSE_ECMULT_STATIC_PRECOMPUTATION /DECMULT_WINDOW_SIZE=16 /DWALLY_CORE_BUILD %ELEMENTS_OPT% /DHAVE_CONFIG_H /DSECP256K1_BUILD /I%LIBWALLY_DIR%\src\wrap_js\windows_config /I%LIBWALLY_DIR% /I%LIBWALLY_DIR%\src /I%LIBWALLY_DIR%\include /I%LIBWALLY_DIR%\src\ccan /I%LIBWALLY_DIR%\src\ccan\base64 /I%LIBWALLY_DIR%\src\secp256k1 /Zi /LD src/aes.c src/base58.c src/bech32.c src/bip32.c src/bip38.c src/bip39.c src/blech32.c src/descriptor.c src/ecdh.c src/elements.c src/hex.c src/hmac.c src/internal.c src/mnemonic.c src/pbkdf2.c src/psbt.c src/script.c src/scrypt.c src/sign.c src/symmetric.c src/transaction.c src/wif.c src/wordlist.c src/ccan/ccan/crypto/ripemd160/ripemd160.c src/ccan/ccan/crypto/sha256/sha256.c src/ccan/ccan/crypto/sha512/sha512.c src/ccan/ccan/base64/base64.c src\ccan\ccan\str\hex\hex_.c src/secp256k1/src/secp256k1.c /Fewally.dll
