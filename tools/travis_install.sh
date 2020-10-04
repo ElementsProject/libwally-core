85a5f7e1ea1c7d3c310da6019097286ac8fb90f5
Jon Griffiths <jon_p_griffiths@yahoo.com>
2020-10-05 12:06:35 +1300

    travis: use the built in brew install mechanism

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew update
-    brew install gnu-sed
-    brew install swig yarn
-elif [ "$TRAVIS_OS_NAME" = "windows" ]; then
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
     SWIG_VERSION="4.0.1"
     choco install swig --version $SWIG_VERSION
     choco install python --version 3.7.4
