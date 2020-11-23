c001a2b606e999bf22318fe5fba1e40b0dcf138c
Lawrence Nahum <lawrence@greenaddress.it>
2020-11-23 18:12:17 +0100

    update to nodejs 12

--- contrib/buster_deps.sh
+++ contrib/buster_deps.sh
@@ -10,7 +10,7 @@ apt-get upgrade -yqq
 apt-get install git uncrustify python{,3}-distutils-extra python{,3}-dev build-essential libffi-dev swig autoconf libtool pkg-config lib32z1 openjdk-11-jdk ca-certificates-java unzip curl libc6:i386 libc6-dev:i386 libncurses5:i386 libstdc++6:i386 lib32z1 virtualenv python{,3}-setuptools apt-transport-https -yqq
 update-java-alternatives -s java-1.11.0-openjdk-amd64
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -qq
