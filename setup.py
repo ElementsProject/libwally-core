7fed231a2f4a1f957b75c7decabe437428888881
Jon Griffiths <jon_p_griffiths@yahoo.com>
2021-10-05 16:50:19 +1300

    Python: Change how setup.py builds the wallycore module
    Fixes the tests to work with the updated module as a result.
    
    Our current approach of building a pure python library and hacking this
    to add the native library no longer works. The latest releases of
    auditwheel now fail because the shared library is not placed in the wheel
    file structure correctly given it's arch-specific (it is placed in the
    purelib folder). As a result, PyPI modules are rejected.
    
    All attempts to hack the internals of setuptool/distutils to work around
    this either this fail to produce a valid module, fail for some
    versions due to internal changes, or fail to work for egg vs wheel
    production.
    
    Instead of fighting the python ext_module system, we now generate the
    sources up front and then use the standard/supported ext_module mechanism
    to build the module. This produces valid eggs/wheels that pass auditwheel
    checking for all versions that we build.

--- setup.py
+++ setup.py
@@ -1,4 +1,65 @@
 """setuptools config for wallycore """
+from setuptools import setup, Extension
+import platform
+
+is_windows = platform.system() == "Windows"
+
+if not is_windows:
+    # Run the autotools/make build up front to generate our sources,
+    # then build using the standard Python ext module machinery.
+    # (Windows requires source generation to be done separately).
+    import multiprocessing
+    import os
+    import subprocess
+
+    abs_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+    def call(cmd):
+        subprocess.check_call(cmd.split(' '), cwd=abs_path)
+
+    call('./tools/cleanup.sh')
+    call('./tools/autogen.sh')
+    call('./configure --enable-swig-python --enable-python-manylinux --enable-ecmult-static-precomputation --enable-elements --disable-tests')
+    call('make -j{}'.format(multiprocessing.cpu_count()))
+
+define_macros=[
+    ('SWIG_PYTHON_BUILD', None),
+    ('WALLY_CORE_BUILD', None),
+    ('HAVE_CONFIG_H', None),
+    ('SECP256K1_BUILD', None),
+    ('BUILD_ELEMENTS', None)
+    ]
+if is_windows:
+    define_macros.append(('USE_ECMULT_STATIC_PRECOMPUTATION', None))
+    define_macros.append(('ECMULT_WINDOW_SIZE', 16))
+
+include_dirs=[
+    './',
+    './src',
+    './include',
+    './src/ccan',
+    './src/secp256k1',
+    './src/secp256k1/src/'
+    ]
+if is_windows:
+    # Borrowing config from wrap_js
+    # TODO: Move this to another directory
+    include_dirs = ['./src/wrap_js/windows_config'] + include_dirs
+
+extra_compile_args = ['-flax-vector-conversions']
+
+wally_ext = Extension(
+    '_wallycore',
+    define_macros=define_macros,
+    include_dirs=include_dirs,
+    extra_compile_args=extra_compile_args,
+    sources=[
+        'src/swig_python/swig_wrap.c' if is_windows else 'src/swig_python/swig_python_wrap.c',
+        'src/wrap_js/src/combined.c',
+        'src/wrap_js/src/combined_ccan.c',
+        'src/wrap_js/src/combined_ccan2.c',
+        ],
+    )
 
 kwargs = {
     'name': 'wallycore',
@@ -10,7 +71,6 @@ kwargs = {
     'author_email': 'jon_p_griffiths@yahoo.com',
     'license': 'MIT',
     'zip_safe': False,
-
     'classifiers': [
         'Development Status :: 5 - Production/Stable',
 
@@ -19,106 +79,21 @@ kwargs = {
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-
     'keywords': 'Bitcoin wallet BIP32 BIP38 BIP39 secp256k1',
     'project_urls': {
         'Documentation': 'https://wally.readthedocs.io/en/latest',
         'Source': 'https://github.com/ElementsProject/libwally-core',
         'Tracker': 'https://github.com/ElementsProject/libwally-core/issues',
     },
-
     'packages': ['wallycore'],
-    'package_dir': {'':'src/swig_python'},
+    'package_dir': {'': 'src/swig_python'},
+    'py_modules': ['wallycore'],
+    'ext_modules': [wally_ext]
 }
-
-import platform
-if platform.system() == "Windows":
-    # On windows wally is defined as a standard python extension
-    from distutils.core import Extension
-
-    wally_ext = Extension(
-        '_wallycore',
-        define_macros=[
-            ('SWIG_PYTHON_BUILD', None),
-            ('USE_ECMULT_STATIC_PRECOMPUTATION', None),
-            ('ECMULT_WINDOW_SIZE', 16),
-            ('WALLY_CORE_BUILD', None),
-            ('HAVE_CONFIG_H', None),
-            ('SECP256K1_BUILD', None),
-            ('BUILD_ELEMENTS', None),
-            ],
-        include_dirs=[
-            # Borrowing config from wrap_js
-            # TODO: Move this to another directory
-            './src/wrap_js/windows_config',
-            './',
-            './src',
-            './include',
-            './src/ccan',
-            './src/secp256k1',
-            './src/secp256k1/src/',
-            ],
-        sources=[
-            'src/swig_python/swig_wrap.c',
-            'src/wrap_js/src/combined.c',
-            'src/wrap_js/src/combined_ccan.c',
-            'src/wrap_js/src/combined_ccan2.c',
-            ],
-    )
-    kwargs['py_modules'] = ['wallycore']
-    kwargs['ext_modules'] = [wally_ext]
-else:
-    # *nix uses a custom autotools/make build
-    import distutils
-    import distutils.command.build_py
-    import os
-    import subprocess
-    import multiprocessing
-
-    class _build_py(distutils.command.build_py.build_py):
-
-        def build_libwallycore(self):
-            abs_path = os.path.dirname(os.path.abspath(__file__)) + '/'
-
-            def call(cmd):
-                subprocess.check_call(cmd.split(' '), cwd=abs_path)
-
-            # Run the autotools/make build to generate a python extension module
-            call('./tools/cleanup.sh')
-            call('./tools/autogen.sh')
-            call('./configure --enable-swig-python --enable-python-manylinux --enable-ecmult-static-precomputation --enable-elements --disable-tests')
-            call('make -j{}'.format(multiprocessing.cpu_count()))
-
-            # Copy the so that has just been built to the build_dir that distutils expects it to be in
-            # The extension of the built lib is dylib on osx
-            so_ext = 'dylib' if platform.system() == 'Darwin' else 'so'
-            src_so = 'src/.libs/libwallycore.{}'.format(so_ext)
-            distutils.dir_util.mkpath(self.build_lib)
-            dest_so = os.path.join(self.build_lib, 'libwallycore.so')
-            distutils.file_util.copy_file(src_so, dest_so)
-
-        def run(self):
-            # Override build_py to first build the c library, then perform the normal python build.
-            # Overriding build_clib would be more obvious but that results in setuptools trying to do
-            # build_py first, which fails because the wallycore/__init__.py is created by making the
-            # clib
-            self.build_libwallycore()
-            distutils.command.build_py.build_py.run(self)
-
-    kwargs['cmdclass'] = {'build_py': _build_py}
-
-    # Force Distribution to have ext modules. This is necessary to generate the correct platform
-    # dependent filename when generating wheels because the building of the underlying wally c libs
-    # is effectively hidden from distutils - which means it assumes it is building a pure python
-    # module.
-    from distutils.dist import Distribution
-    Distribution.has_ext_modules = lambda self: True
-
-from setuptools import setup
 setup(**kwargs)
