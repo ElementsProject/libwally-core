5818eca7f7fb5e70d542de7b91220c9a40965771
Jon Griffiths <jon_p_griffiths@yahoo.com>
2022-06-03 22:43:41 +1200

    tools: add tools/build_psbt_ctests.py to auto-generate the psbt ctest cases
    Re-generate src/ctest/psbts.h to pick up the new cases; this increases
    our valgrind coverage of the PSBT code.

---
+++ tools/build_psbt_ctests.py
@@ -0,0 +1,31 @@
+# Generate the PSBT c-test header file src/ctest/psbts.h
+import json
+
+def dump(cases):
+    for case in cases:
+        if case.get('is_pset'):
+            print('#ifdef BUILD_ELEMENTS')
+        print('    /* {} */'.format(case['comment']))
+        print('    {{"{}"}},'.format(case['psbt']))
+        if case.get('is_pset'):
+            print('#endif /* BUILD_ELEMENTS */')
+        if case != cases[-1]:
+            print()
+
+with open('src/data/psbt.json', 'r') as f:
+    JSON = json.load(f)
+
+print('''/* Generated file - do not edit! */
+struct psbt_test {
+    const char *base64;
+};
+
+static const struct psbt_test invalid_psbts[] =
+{''')
+dump(JSON['invalid'])
+print('''};
+
+static const struct psbt_test valid_psbts[] =
+{''')
+dump(JSON['valid'])
+print('};\n')
