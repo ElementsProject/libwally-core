8828e24d755551d79372a2d339a43d85ba52ec70
Nadav Ivgi <nadav@shesek.info>
2022-11-18 00:33:29 +0200

    Update WASM constants and version number as part of update_generated.sh

--- tools/update_generated.sh
+++ tools/update_generated.sh
@@ -2,3 +2,6 @@
 ./tools/build_wrappers.py
 jq . src/data/psbt.json >.foo && mv .foo src/data/psbt.json
 ./tools/build_psbt_ctests.py >src/ctest/psbts.h
+
+# Update WASM package constants and version
+./tools/update_wasm_package.sh
