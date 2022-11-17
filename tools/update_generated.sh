f2a496e4bf905e144479b7e3e3ab1951e2abdd55
Nadav Ivgi <nadav@shesek.info>
2022-11-17 07:26:49 +0200

    Expose internal functions in the WASM package
    - Support was added for DestPtr to a single (non-array) uint32_t,
      because it was necessary for one of the internal functions
      (wally_psbt_get_output_blinder_index, the only function with such an
      argument).
    
    - build_wrapprs.py was refactored to update both the internal and
      external functions in the same execution, instead of having to run it
      twice. This simplifies some things for the WASM codegen.

--- tools/update_generated.sh
+++ tools/update_generated.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-./tools/build_wrappers.py --internal
 ./tools/build_wrappers.py
 jq . src/data/psbt.json >.foo && mv .foo src/data/psbt.json
 ./tools/build_psbt_ctests.py >src/ctest/psbts.h
