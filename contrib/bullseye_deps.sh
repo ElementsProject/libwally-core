2181457c17100bbb6b07bedc753d55940a92ecbe
Nadav Ivgi <nadav@shesek.info>
2022-11-20 01:17:56 +0200

    Update to nodejs v16.x and emsdk v3.1.20
    (Cherry-picked from https://github.com/ElementsProject/libwally-core/pull/342)

--- contrib/bullseye_deps.sh
+++ contrib/bullseye_deps.sh
@@ -10,7 +10,7 @@ apt-get upgrade -yqq
 apt install --no-install-recommends unzip autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev,-pip,-virtualenv,-distutils} python{,-dev}-is-python3 clang{,-format,-tidy} git swig openjdk-11-jdk g++-mingw-w64-x86-64 curl -yqq
 update-java-alternatives -s java-1.11.0-openjdk-amd64
 
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -qq
@@ -20,10 +20,10 @@ apt-get install -yqq nodejs yarn
 cd /opt && curl -sSO https://dl.google.com/android/repository/${NDK_FILENAME}
 unzip -qq ${NDK_FILENAME}
 rm ${NDK_FILENAME}
-git clone https://github.com/juj/emsdk.git
+git clone https://github.com/emscripten-core/emsdk
 cd emsdk
-./emsdk install 2.0.9
-./emsdk activate 2.0.9
+./emsdk install 3.1.20
+./emsdk activate 3.1.20
 source ./emsdk_env.sh
 
 apt-get remove --purge curl unzip -yqq
