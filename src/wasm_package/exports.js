3fecdca8138ecd949900ab9a98bf6e5d1de26d9f
Nadav Ivgi <nadav@shesek.info>
2022-11-28 20:38:14 +0200

    wasm: Make all named exports available via the default export
    With this, its possible to use the more idiomatic `import wally from
    'wallycore'`, rather than `import * as wally from 'wallycore'`.
    
    The `* as` style is still supported.

---
+++ src/wasm_package/exports.js
@@ -0,0 +1,3 @@
+export * from './core.js'
+export * from './const.js'
+export * from './functions.js'
--- src/wasm_package/index.js
+++ src/wasm_package/index.js
@@ -1,3 +1,6 @@
-export * from './core.js'
-export * from './const.js'
-export * from './functions.js'
+// Combining functions.js + const.js + core.js as the default export requires a separate
+// exports.js file that unifies them as named exports.
+// With this, library users can `import wally from 'wallycore'`, without `* as`
+
+export * from './exports.js'
+export * as default from './exports.js'
