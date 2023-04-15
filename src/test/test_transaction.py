870c0207084f76d8e08499b6af7f5c578d54d85a
Jon Griffiths <jon_p_griffiths@yahoo.com>
2023-04-15 19:01:12 +1200

    tx: skip hashPrevouts test for non-elements builds

--- src/test/test_transaction.py
+++ src/test/test_transaction.py
@@ -266,6 +266,13 @@ class TransactionTests(unittest.TestCase):
 
     def test_hash_prevouts(self):
         """Test functions computing hash_prevouts"""
+        if not wally_is_elements_build()[1]:
+            # The direct access tx.num_inputs/tx.inputs[i].txhash/tx.inputs[i].index
+            # below only works if this is an elements build. Skip this test for
+            # non-elements builds until the tx accessors are available to call
+            # when SWIG is not defined.
+            self.skipTest('https://github.com/ElementsProject/libwally-core/issues/388')
+
         out, out_len = make_cbuffer('00'*32)
         # The first sample tx from BIP-0143
         bip143_tx = '0100000002fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000000eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac11000000'
