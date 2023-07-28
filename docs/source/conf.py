3d2f0d491ff440fd6b99159cb4aa3fac3e00c0b3
Jon Griffiths <jon_p_griffiths@yahoo.com>
2023-07-28 13:39:09 +1200

    Bump version to 0.9.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -167,7 +167,7 @@ author = u'Jon Griffiths'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.9.0'
+version = u'0.9.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ wally_ext = Extension(
 
 kwargs = {
     'name': 'wallycore',
-    'version': '0.9.0',
+    'version': '0.9.1',
     'description': 'libwally Bitcoin library',
     'long_description': 'Python bindings for the libwally Bitcoin library',
     'url': 'https://github.com/ElementsProject/libwally-core',
