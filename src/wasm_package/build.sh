ee98e264e410683f1ae72c6032b9d48c4c93db98
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-03-20 07:45:33 +1300

    js: set emcc options when generating for the npm package

--- src/wasm_package/build.sh
+++ src/wasm_package/build.sh
@@ -2,7 +2,7 @@
 set -xeo pipefail
 
 # Build WASM (Note Elements is always enabled)
-(cd ../.. && ./tools/build_wasm.sh)
+(cd ../.. && EMCC_OPTIONS="-s EXPORT_ES6=1 -s MODULARIZE=1" ./tools/build_wasm.sh)
 mkdir -p libwally_wasm && cp ../../dist/wallycore.{js,wasm} libwally_wasm/
 touch libwally_wasm/index # necessary for webpack to work (fixes "Can't resolve './' in 'wasm_package/libwally_wasm'")
 
