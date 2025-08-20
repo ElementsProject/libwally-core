a445157d180c5d67d7f6f0d8abe9c84d956d8dad
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-08-20 21:00:53 +1200

    Bump version to 1.5.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -168,7 +168,7 @@ author = u'Jon Griffiths'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.5.0'
+version = u'1.5.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
